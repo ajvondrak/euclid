cfdd18d228e618dfe20b0bf7c28513c67a57c5b3
Alex Vondrak <ajvondrak@gmail.com>
2021-03-10 11:19:22 -0800

    Use printf consistently
    It's weird having the git ref be the only exception. But also this
    generalizes the formatting strings so they can take in extra info (once
    we start feeding it to them).

--- euclid.zsh
+++ euclid.zsh
@@ -5,31 +5,31 @@ fi
 typeset -gA EUCLID
 
 EUCLID=(
-  [LOGO]="%F{default}\ufa62%f"
-  [PATH]="%F{green}%~%f "
-  [VICMD]="%F{214}\ufa62%f"
-  [ERROR]="%F{red}\ufa62%f"
+  [LOGO]="%%F{default}\ufa62%%f"
+  [PATH]="%%F{green}%%~%%f "
+  [VICMD]="%%F{214}\ufa62%%f"
+  [ERROR]="%%F{red}\ufa62%%f"
   [REF]="\uf417 %%F{242}%s%%f"
   [EVEN]=""
-  [AHEAD]="%F{214}+%f"
-  [BEHIND]="%F{214}-%f"
-  [DIVERGED]="%F{214}!%f"
-  [CLEAN]=" %F{green}\uf7d7%f"
-  [STAGED]=" %F{green}\uf7d8%f"
-  [UNSTAGED]=" %F{red}\uf7d8%f"
-  [CONFLICT]=" %F{red}\uf7d7%f"
-  [STASH]=" %F{blue}\uf461%f"
+  [AHEAD]="%%F{214}+%%f"
+  [BEHIND]="%%F{214}-%%f"
+  [DIVERGED]="%%F{214}!%%f"
+  [CLEAN]=" %%F{green}\uf7d7%%f"
+  [STAGED]=" %%F{green}\uf7d8%%f"
+  [UNSTAGED]=" %%F{red}\uf7d8%%f"
+  [CONFLICT]=" %%F{red}\uf7d7%%f"
+  [STASH]=" %%F{blue}\uf461%%f"
 )
 
 euclid::logo() {
   case "$KEYMAP" in
-    vicmd) echo -n "${EUCLID[VICMD]}" ;;
-    *) echo -n "%(?.${EUCLID[LOGO]}.${EUCLID[ERROR]})" ;;
+    vicmd) printf "${EUCLID[VICMD]}" ;;
+    *) printf "%%(?.${EUCLID[LOGO]}.${EUCLID[ERROR]})" ;;
   esac
 }
 
 euclid::path() {
-  echo -n "${EUCLID[PATH]}"
+  printf "${EUCLID[PATH]}"
 }
 
 gitstatus_stop 'euclid' && gitstatus_start 'euclid'
@@ -50,13 +50,13 @@ euclid::tracking() {
   if [[ "$VCS_STATUS_RESULT" != 'ok-sync' ]]; then
     return 0
   elif (( !VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )); then
-    echo -n "${EUCLID[EVEN]}"
+    printf "${EUCLID[EVEN]}"
   elif (( !VCS_STATUS_COMMITS_AHEAD && VCS_STATUS_COMMITS_BEHIND )); then
-    echo -n "${EUCLID[BEHIND]}"
+    printf "${EUCLID[BEHIND]}"
   elif (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )); then
-    echo -n "${EUCLID[AHEAD]}"
+    printf "${EUCLID[AHEAD]}"
   elif (( VCS_STATUS_COMMITS_AHEAD && VCS_STATUS_COMMITS_BEHIND )); then
-    echo -n "${EUCLID[DIVERGED]}"
+    printf "${EUCLID[DIVERGED]}"
   fi
 }
 
@@ -65,13 +65,13 @@ euclid::staging() {
   if [[ "$VCS_STATUS_RESULT" != 'ok-sync' ]]; then
     return 0
   elif (( VCS_STATUS_HAS_CONFLICTED )); then
-    echo -n "${EUCLID[CONFLICT]}"
+    printf "${EUCLID[CONFLICT]}"
   elif (( !VCS_STATUS_HAS_UNSTAGED && !VCS_STATUS_HAS_STAGED )); then
-    echo -n "${EUCLID[CLEAN]}"
+    printf "${EUCLID[CLEAN]}"
   elif (( VCS_STATUS_HAS_STAGED )); then
-    echo -n "${EUCLID[STAGED]}"
+    printf "${EUCLID[STAGED]}"
   else
-    echo -n "${EUCLID[UNSTAGED]}"
+    printf "${EUCLID[UNSTAGED]}"
   fi
 }
 
@@ -80,7 +80,7 @@ euclid::stash() {
   if [[ "$VCS_STATUS_RESULT" != 'ok-sync' ]]; then
     return 0
   elif (( VCS_STATUS_STASHES )); then
-    echo -n "${EUCLID[STASH]}"
+    printf "${EUCLID[STASH]}"
   fi
 }
 
